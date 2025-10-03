1062f062263be655520ad4d41b96b9c3ee832e48
Natalia Kozlova <sleeplesspony@gmail.com>
2025-10-03 13:21:00 +0300

    add fixes for comparing of flat json files

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/sleeplesspony/php-project-48/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/sleeplesspony/php-project-48/actions)
+
+## Prerequisites
+
+* Linux, Macos, WSL
+* PHP >=8.2
+* Make
+* Git
+* Composer
+
+## Demo
+
+See GenDiff in action
+
+### Even Number:
+https://asciinema.org/a/FnGWnM7hrFzOpYK9xdiBIZZ0X
+
--- src/Parser.php
+++ src/Parser.php
@@ -1,28 +1,35 @@
 <?php
 
-namespace Gendiff\Parser;
+namespace Differ\Parser;
+use Funct\Collection;
 
-function printFileContent($filePath) {
+function getFileContent(string $filePath): string {
 
     $absolutePath = realpath($filePath);
     if ($absolutePath === false) {
+        throw new \Exception("File not found: {$filePath}");
+    }
 
-        print_r("File not found: {$filePath}");
+    $fileContent = file_get_contents($absolutePath);
+    if ($fileContent === false) {
+        throw new \Exception("Can not read file: {$filePath}");
+    }
 
-    } else {
+    return $fileContent;
 
-        $fileName = basename($absolutePath);
+}
 
-        $fileContent = file_get_contents($absolutePath);
-        $data = json_decode($fileContent);
-        print_r($fileName . "\n\n");
-        foreach($data as $key => $value) {
-            if (is_bool($value)) {
-                $value = $value ? "true" : "false";
-            }
-            print_r("{$key} : {$value}\n");
+function printFileContent(string $filePath): void {
+
+    $fileContent = getFileContent($filePath);
+    $fileName = basename($filePath);
+    $data = json_decode($fileContent);
+    print_r($fileName . "\n\n");
+    foreach($data as $key => $value) {
+        if (is_bool($value)) {
+            $value = $value ? "true" : "false";
         }
-
+        print_r("{$key} : {$value}\n");
     }
 
     print_r("\n\n");
