6567d72aecd7f980c23b19a1eb4087f75843a7b4
Natalia Kozlova <sleeplesspony@gmail.com>
2025-10-23 12:04:55 +0300

    refactor of gendiff function

--- src/Differ.php
+++ src/Differ.php
@@ -2,9 +2,10 @@
 
 namespace Differ\Differ;
 
-use Differ\Parser;
-use Differ\Formatters;
-use Funct\Collection;
+use function Differ\Parser\getFileData;
+use function Differ\Formatters\formatResult;
+use function Funct\Collection\union;
+use function Funct\Collection\sortBy;
 
 const STATUS_NOT_CHANGED = 'not-changed';
 const STATUS_CHANGED = 'changed';
@@ -12,23 +13,19 @@ const STATUS_REMOVED = 'removed';
 const STATUS_ADDED = 'added';
 const STATUS_PARENT = 'parent';
 
-function genDiff(string $pathToFile1, string $pathToFile2, $format = "stylish"): string
+function genDiff(string $pathToFile1, string $pathToFile2, string $format = "stylish"): string
 {
-    $file1Content = Parser\getFileContent($pathToFile1);
-    $file2Content = Parser\getFileContent($pathToFile2);
-
-    $data1 = Parser\parse(pathinfo($pathToFile1)["extension"], $file1Content);
-    $data2 = Parser\parse(pathinfo($pathToFile2)["extension"], $file2Content);
-
-    return Formatters\formatResult(getDiffTree($data1, $data2), $format);
+    $data1 = getFileData($pathToFile1);
+    $data2 = getFileData($pathToFile2);
+    return formatResult(getDiffTree($data1, $data2), $format);
 }
 
 function getDiffTree(array $data1, array $data2): array
 {
     $keys1 = array_keys($data1);
     $keys2 = array_keys($data2);
-    $keys = Collection\union($keys1, $keys2);
-    $keys = Collection\sortBy($keys, fn($key) => $key);
+    $keys = union($keys1, $keys2);
+    $keys = sortBy($keys, fn($key) => $key);
     $keys = array_values($keys);
 
     $diffTree = [];
--- src/Parser.php
+++ src/Parser.php
@@ -4,6 +4,8 @@ namespace Differ\Parser;
 
 use Symfony\Component\Yaml\Yaml;
 
+const SUPPORTED_EXTENSIONS = [ 'json', 'yaml', 'yml'];
+
 function parseJson(string $dataString): array
 {
     return json_decode($dataString, true, JSON_THROW_ON_ERROR);
@@ -27,27 +29,44 @@ function parse(string $format, string $dataString): array
     return $parse($dataString);
 }
 
-function getFileContent(string $filePath): string
+function getFileData(string $pathToFile): array
+{
+    $fileContent = getFileContent($pathToFile);
+    $extension = getFileExtension($pathToFile);
+    return parse($extension, $fileContent);
+}
+
+function getFileContent(string $pathToFile): string
 {
 
-    $absolutePath = realpath($filePath);
+    $absolutePath = realpath($pathToFile);
     if ($absolutePath === false) {
-        throw new \UnexpectedValueException("File not found: {$filePath}");
+        throw new \UnexpectedValueException("File not found: {$pathToFile}");
     }
 
     $fileContent = file_get_contents($absolutePath);
     if ($fileContent === false) {
-        throw new \UnexpectedValueException("Can not read file: {$filePath}");
+        throw new \UnexpectedValueException("Can not read file: {$pathToFile}");
     }
 
     return $fileContent;
 }
 
-function printFileContent(string $filePath): void
+function getFileExtension(string $pathToFile): string
+{
+    $extension = pathinfo($pathToFile, PATHINFO_EXTENSION);
+    if (in_array($extension, SUPPORTED_EXTENSIONS)) {
+        return $extension;
+    } else {
+        throw new \UnexpectedValueException("Unsupported file extension: {$extension}");
+    }
+}
+
+function printFileContent(string $pathToFile): void
 {
 
-    $fileContent = getFileContent($filePath);
-    $fileName = basename($filePath);
+    $fileContent = getFileContent($pathToFile);
+    $fileName = basename($pathToFile);
     $data = json_decode($fileContent);
     print_r($fileName . "\n\n");
     foreach ($data as $key => $value) {
