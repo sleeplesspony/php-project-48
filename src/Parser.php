5fdff7afdc4e537c0c514be64a869a2fd307e70c
Natalia Kozlova <sleeplesspony@gmail.com>
2025-10-24 14:03:16 +0300

    refactoring for Parse.php

--- src/Parser.php
+++ src/Parser.php
@@ -6,21 +6,14 @@ use Symfony\Component\Yaml\Yaml;
 
 function parse(string $format, string $dataString): array
 {
-    if ($format === "json") {
-        return parseJson($dataString);
-    } elseif ($format === "yaml" || $format === "yml") {
-        return parseYaml($dataString);
+
+    switch ($format) {
+        case 'json':
+            return json_decode($dataString, true, JSON_THROW_ON_ERROR);
+        case 'yaml':
+        case 'yml':
+            return Yaml::parse($dataString);
+        default:
+            throw new \UnexpectedValueException("Unsupported file format: {$format}");
     }
-
-    throw new \UnexpectedValueException("Unsupported file format: {$format}");
-}
-
-function parseJson(string $dataString): array
-{
-    return json_decode($dataString, true, JSON_THROW_ON_ERROR);
-}
-
-function parseYaml(string $dataString): array
-{
-    return Yaml::parse($dataString);
 }
